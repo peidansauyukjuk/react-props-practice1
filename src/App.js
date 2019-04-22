5ea2ca4b3e54471585d4533c6c0d1c5c6af1a6c6
Simon Mei <smei8800@gmail.com>
2019-04-22 14:59:36 -0400

    prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from './Components/Header.js'
-import Artist from './Components/Artist.js'
+import Header from "./Components/Header.js";
+import Artist from "./Components/Artist.js";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header header="Header"/>;
-        <Artist image="https://www.denverpost.com/wp-content/uploads/2016/03/20151116_the_lumineers_shot_03_054.jpg?w=460" 
-        name="The Lumineers" songName="Ophelia" alt="The Lumineers"/>
+        <Header header="Header" />;
+        <Artist
+          image="https://www.denverpost.com/wp-content/uploads/2016/03/20151116_the_lumineers_shot_03_054.jpg?w=460"
+          name="The Lumineers"
+          songName="Ophelia"
+          alt="The Lumineers"
+        />
       </div>
-    )
+    );
   }
 }
 
